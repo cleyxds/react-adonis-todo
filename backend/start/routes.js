7eff43acbc9b11605bdcc74e01f410e4691d8131
cleyxds <cleysonn@hotmail.com.br>
2020-03-30 06:41:54 -0300

    create and index projects

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -18,7 +18,9 @@ const Route = use('Route')
 
 Route.group(() => {
   Route.post('auth/register', 'UserController.create');
-  Route.get('auth/login', 'UserController.login');
-
+  Route.post('auth/login', 'UserController.login');
+  Route.get('/projects', 'ProjectController.index').middleware('auth');
+  Route.post('/projects', 'ProjectController.create').middleware('auth');
+  
 }).prefix('/api')
 
