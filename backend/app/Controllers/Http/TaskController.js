fd238d5b6369c093896a35400309a5a5be483673
cleyxds <cleysonn@hotmail.com.br>
2020-03-30 08:13:41 -0300

    tasks index and create

---
+++ backend/app/Controllers/Http/TaskController.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Project = use('App/Models/Project');
+const Task = use('App/Models/Task');
+const AuthorizationService = use('App/Services/AuthorizationService');
+
+class TaskController {
+  async index({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+
+    AuthorizationService(project, user);
+
+    return await project.tasks().fetch();
+  }
+
+  async create({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { description } = request.body;
+    const { id } = params;
+    const project = await Project.find(id);
+
+    AuthorizationService(project, user);
+    const task = new Task();
+
+    task.fill({
+      description
+    });
+
+    await project.tasks().save(task);
+
+    return task;
+  }
+}
+
+module.exports = TaskController
